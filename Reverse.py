290e714b151c91ef1b6b17e381651f8e438d4c47
Shubh0065 <145219840+Shubh0065@users.noreply.github.com>
2023-09-18 21:13:23 +0530

    Create Reverse.py

---
+++ Reverse.py
@@ -0,0 +1,17 @@
+def reverse_st(st):
+
+    str = " "
+
+    for i in st :
+
+        str = i + str
+
+    return str   
+
+
+
+st = "shubham kumar"
+
+print(st)
+
+print("The reverse string is " , reverse_st(st))
