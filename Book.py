2340a1e5acb01f646f54aa9519aa68a8158aa7b2
Shubh0065 <145219840+Shubh0065@users.noreply.github.com>
2023-09-18 21:09:25 +0530

    Create Book.py

---
+++ Book.py
@@ -0,0 +1,17 @@
+class books:
+
+    name = 'let us c'
+
+    author = 'yashwant'
+
+    def display(self):
+
+        print("name of book :" , self.name)
+
+        print("name of author :", self.author)
+
+
+
+b1 = books()
+
+b1.display()
